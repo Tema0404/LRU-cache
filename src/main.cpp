d2ea563e78163aaae168b26f98950277931128c4
Artyom <artm.nikolayev.2008@inbox.ru>
2026-05-30 18:25:40 +0300

    Remove unused array and improve code formatting
    Remove unused array declaration and clean up formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include "LRUCache.h"
 
-
 int main(){
     LRUCache<int, int> cache(3);
-    int arr[]{1, 2, 3, 4};
     for(int i = 0; i < 4; i++){
         for(int j = 4; j < 9; j++){
             cache.put(i, j);
@@ -12,5 +10,4 @@ int main(){
         }
     }
     cache.cache_info();
-
 }
